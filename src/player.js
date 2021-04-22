6a1524d00b92088a4b6c4b8775c0630fae83eb04
Ashton Huxtable <aehuxtable@gmail.com>
2021-04-22 14:18:20 -0600

    Create player class and add methods

---
+++ src/player.js
@@ -0,0 +1,16 @@
+class Player {
+  constructor(thePlayer) {
+    this.name = thePlayer.name;
+    this.token = thePlayer.token;
+    this.wins = thePlayer.wins;
+  }
+  saveWinsToStorage(){
+
+  };
+  retrieveWinsFromStorage() {
+
+  };
+  takeTurn() {
+
+  };
+}
