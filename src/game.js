dd5393f77686b99ece827bbb57a09eb640f24df2
Ashton Huxtable <aehuxtable@gmail.com>
2021-04-22 15:24:44 -0600

    Create game class and pseudocode for potential logic needed

---
+++ src/game.js
@@ -0,0 +1,36 @@
+class Game {
+  constructor(gameType){
+    this.players = [];
+    this.gameType = gameType;
+
+
+
+  }
+  determineWinner() {
+    // method that checks the existing data on the game board
+    // and checks the rules for the game being played
+    // and returns the winner.
+    // increments the wins by 1
+    // assigns wins to player
+    // logic for difficult
+    // logic for easy
+  }
+
+  isGameDraw() {}
+  // isGameDraw() method that checks to see if
+  // one player one and evaluates to true if not
+  // in main JS condition that says if isGameDraw === true
+  // then replace subtitle innertext with 'draw'
+
+  }
+  resetGame(){
+    // method that resets the game board???
+  }
+}
+
+// computer gets random index when player 1
+// chooses a player
+
+// getRandomIndex(array) {
+// return math.floor(math.random() * array.length)
+//}
