160d67c99a8d1f5b957753ac66e092ebbdd662d9
Ashton Huxtable <aehuxtable@gmail.com>
2021-04-22 18:22:23 -0600

    Add functionality to buttons to change to difficult game

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 // QUERY SELECTORS
 var classicBtn = document.getElementById('classic')
+var difficultBtn = document.getElementById('difficult')
 var gameConsole = document.getElementById('gameConsole')
+
 
 // EVENT LISTENERS
 classicBtn.addEventListener('click', displayClassicChoices)
+difficultBtn.addEventListener('click', displayDifficultChoices)
 
 // EVENT HANDLERS
 
@@ -16,4 +19,18 @@ function displayClassicChoices() {
     <img class='classic-fighter' id='classicScissors' src='./assets/happy-scissors.png' alt='happy scissors'>
   </section>
   `
+}
+
+function displayDifficultChoices() {
+  gameConsole.innerHTML = `
+  <h3>Choose your fighter!</h3>
+  <section class='difficult-choices'>
+    <img class='difficult-fighter' id='difficultRock' src='./assets/happy-rocks.png' alt='happy rocks icon'>
+    <img class='difficult-fighter' id='difficultAlien' src='./assets/flat-alien.png' alt='alien icon'>
+    <img class='difficult-fighter' id='difficultLizard' src='./assets/lizard.png' alt='lizard icon'>
+    <img class='difficult-fighter' id='difficultCave' src='./assets/cave.png' alt='cave icon'>
+    <img class='difficult-fighter' id='difficultUFO' src='./assets/ufo.png' alt='ufo icon'>
+  </section>
+  `
+
 }
