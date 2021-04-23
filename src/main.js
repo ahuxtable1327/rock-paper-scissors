ec114e2a3e21e361dbad40046c25252d3eccb554
Ashton Huxtable <aehuxtable@gmail.com>
2021-04-22 20:08:15 -0600

    Update character choices to be buttons

--- src/main.js
+++ src/main.js
@@ -1,22 +1,39 @@
 // QUERY SELECTORS
-var classicBtn = document.getElementById('classic')
-var difficultBtn = document.getElementById('difficult')
 var gameConsole = document.getElementById('gameConsole')
 
 
 // EVENT LISTENERS
-classicBtn.addEventListener('click', displayClassicChoices)
-difficultBtn.addEventListener('click', displayDifficultChoices)
+gameConsole.addEventListener('click', function(event) {
+  displayChoices(event)
+});
 
 // EVENT HANDLERS
+
+function displayChoices(event) {
+  if (event.target.id === 'classic') {
+    displayClassicChoices();
+  }
+  if (event.target.id === 'difficult') {
+    displayDifficultChoices();
+  }
+}
 
 function displayClassicChoices() {
   gameConsole.innerHTML = `
   <h3>Choose your fighter!</h3>
   <section class='classic-choices'>
-    <img class='classic-fighter' id='classicRock' src='./assets/happy-rocks.png' alt='happy rocks'>
-    <img class='classic-fighter' id='classicPaper' src='./assets/happy-paper.png' alt='happy paper'>
-    <img class='classic-fighter' id='classicScissors' src='./assets/happy-scissors.png' alt='happy scissors'>
+    <label class='classics'>
+      <input type="radio" name="classics" id='rock'>
+      <img class='classic-fighter' id='classicRock' src='./assets/happy-rocks.png' alt='happy rocks'>
+    </label>
+    <label class='classics'>
+      <input type="radio" name="classics" id='paper'>
+      <img class='classic-fighter' id='classicPaper' src='./assets/happy-paper.png' alt='happy paper'>
+    </label>
+    <label class='classics'>
+      <input type="radio" name="classics" id='scissors'>
+      <img class='classic-fighter' id='classicScissors' src='./assets/happy-scissors.png' alt='happy scissors'>
+    </label>
   </section>
   `
 }
@@ -25,11 +42,26 @@ function displayDifficultChoices() {
   gameConsole.innerHTML = `
   <h3>Choose your fighter!</h3>
   <section class='difficult-choices'>
-    <img class='difficult-fighter' id='difficultCorn' src='./assets/corn.png' alt='Corn icon'>
-    <img class='difficult-fighter' id='difficultCarrot' src='./assets/carrot.png' alt='Carrot icon'>
-    <img class='difficult-fighter' id='difficultTurnip' src='./assets/turnip.png' alt='Turnip icon'>
-    <img class='difficult-fighter' id='difficultTomato' src='./assets/tomato.png' alt='Tomato icon'>
-    <img class='difficult-fighter' id='difficultPotato' src='./assets/potato.png' alt='Potato icon'>
+    <label class='difficult'>
+      <input type="radio" name="hard" id='corn'>
+      <img class='difficult-fighter' id='difficultCorn' src='./assets/corn.png' alt='Corn icon'>
+    </label>
+    <label class='difficult'>
+      <input type="radio" name="hard" id='carrot'>
+      <img class='difficult-fighter' id='difficultCarrot' src='./assets/carrot.png' alt='Carrot icon'>
+    </label>
+    <label class='difficult'>
+      <input type="radio" name="hard" id='turnip'>
+      <img class='difficult-fighter' id='difficultTurnip' src='./assets/turnip.png' alt='Turnip icon'>
+    </label>
+    <label class='difficult'>
+      <input type="radio" name="hard" id='tomato'>
+      <img class='difficult-fighter' id='difficultTomato' src='./assets/tomato.png' alt='Tomato icon'>
+    </label>
+    <label class='difficult'>
+      <input type="radio" name="hard" id='potato'>
+      <img class='difficult-fighter' id='difficultPotato' src='./assets/potato.png' alt='Potato icon'>
+    </label>
   </section>
   `
 }
